f9e0fcbd7af7faa3cb152ef618a8e52990c8256d
OGEmanuel <1blacksanta416@gmail.com>
2024-06-30 21:00:39 +0100

    Particles complete

--- src/script.js
+++ src/script.js
@@ -4,34 +4,17 @@ import {
   MapControls,
   OrbitControls,
 } from "three/examples/jsm/controls/OrbitControls";
+// import * as dat from "dat.gui";
 
 //Scene
 const scene = new THREE.Scene();
 
-// Lights
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-const pointLight = new THREE.PointLight(0xffffff, 0.5);
-pointLight.position.set(2, 2, 2);
-scene.add(ambientLight, pointLight);
-
 // TextureLoader
 const textureLoader = new THREE.TextureLoader();
-const colorTexture = textureLoader.load("/texture/color.jpg");
-const matCapTexture = textureLoader.load("/texture/mat2.png");
-const bumpTexture = textureLoader.load("/texture/bump.jpg");
-const displacementTexture = textureLoader.load("/texture/displacementMap.jpg");
+const particleTexture = textureLoader.load("textures/alphaSnow.jpg"); // Texture should not be more that 40kb
 
-// Cube TextureLoader
-const cubeTextureLoader = new THREE.CubeTextureLoader();
-const envTexture = cubeTextureLoader.load([
-  "/texture/env/px.png",
-  "texture/env/nx.png",
-  "texture/env/py.png",
-  "texture/env/ny.png",
-  "texture/env/pz.png",
-  "texture/env/nz.png",
-]);
-scene.background = envTexture;
+//Debugging
+// const gui = new dat.GUI();
 
 //Resizing
 window.addEventListener("resize", () => {
@@ -49,61 +32,48 @@ window.addEventListener("resize", () => {
 });
 
 //Mesh
-const geometry = new THREE.SphereGeometry(0.5, 32, 32);
-// const geometry = new THREE.TorusGeometry(0.3, 0.2, 32, 32);
-const material = new THREE.MeshStandardMaterial({
-  metalness: 0.9,
-  roughness: 0.1,
-  envMap: envTexture,
-  //   shininess: 200,
-  //   specular: new THREE.Color("green"),
-  //   matcap: matCapTexture,
-  //   map: colorTexture,
-  //   bumpMap: bumpTexture,
-  //   displacementMap: displacementTexture,
-  //   wireframe: true,
-  //   color: new THREE.Color("skyblue"),
-  //   transparent: true,
-  //   opacity: 0.4,
-  //   side: THREE.DoubleSide,
-  //   visible: false,
-});
-// material.map = colorTexture;
-// material.wireframe = true;
-// material.color = new THREE.Color(0xff0000);
-// material.opacity = 0.4
-// material.transparent = true
-// material.side = THREE.FrontSide
-// material.visible = false
-// material.matcap = matcapTexture
-// material.shininess = 200
-// material.specular = 200
+const geometry = new THREE.BufferGeometry();
+const verticesAmount = 10000;
+const positionArray = new Float32Array(verticesAmount * 3); // We need 3000 slot
+for (let i = 0; i < verticesAmount * 3; i++) {
+  positionArray[i] = (Math.random() - 0.5) * 4;
+}
+geometry.setAttribute("position", new THREE.BufferAttribute(positionArray, 3));
 
-// const geometry2 = new THREE.PlaneGeometry(1, 1, 64, 64);
-// const material2 = new THREE.MeshBasicMaterial();
-// const mesh2 = new THREE.Mesh(geometry, material);
-// mesh2.position.z = -1;
-
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const material = new THREE.PointsMaterial();
+material.size = 0.02;
+material.transparent = true;
+material.alphaMap = particleTexture;
+material.depthTest = false;
+const points = new THREE.Points(geometry, material);
+scene.add(points);
 
 //Camera
 const aspect = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
-const camera = new THREE.PerspectiveCamera(75, aspect.width / aspect.height);
-camera.position.z = 1;
+const camera = new THREE.PerspectiveCamera(
+  75,
+  aspect.width / aspect.height,
+  0.01,
+  100
+);
+camera.position.z = 2;
 scene.add(camera);
 
 //Renderer
 const canvas = document.querySelector(".draw");
-const renderer = new THREE.WebGLRenderer({ canvas });
+const renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
 renderer.setSize(aspect.width, aspect.height);
 
 //OrbitControls
 const orbitControls = new OrbitControls(camera, canvas);
 orbitControls.enableDamping = true;
+orbitControls.enableZoom = false;
+orbitControls.enableRotate = false;
+orbitControls.autoRotate = true;
+orbitControls.autoRotateSpeed = 0.2;
 
 //Clock Class
 const clock = new THREE.Clock();
@@ -111,6 +81,10 @@ const clock = new THREE.Clock();
 const animate = () => {
   //GetElapsedTime
   const elapsedTime = clock.getElapsedTime();
+
+  //   animate particles
+  //   points.rotation.y = elapsedTime * 0.05;
+  //   points.rotation.x = elapsedTime * 0.05;
 
   //Update Controls
   orbitControls.update();
