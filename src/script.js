4b6a47d36b326c07ca28e5c02d9e0fc0aba49f62
OGEmanuel <1blacksanta416@gmail.com>
2024-06-30 20:15:06 +0100

    Materials complete

--- src/script.js
+++ src/script.js
@@ -8,26 +8,30 @@ import {
 //Scene
 const scene = new THREE.Scene();
 
-// LoadingManager
-const loadingManager = new THREE.LoadingManager();
-loadingManager.onStart = () => {
-  console.log("Start");
-};
-loadingManager.onLoad = () => {
-  console.log("Loading...");
-};
-loadingManager.onProgress = () => {
-  console.log("Progress");
-};
-loadingManager.onError = () => {
-  console.log("Error!");
-};
+// Lights
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+const pointLight = new THREE.PointLight(0xffffff, 0.5);
+pointLight.position.set(2, 2, 2);
+scene.add(ambientLight, pointLight);
 
-// textureLoader
-// const texture = new THREE.TextureLoader().load("/texture/color.jpg");
-const textureLoader = new THREE.TextureLoader(loadingManager);
+// TextureLoader
+const textureLoader = new THREE.TextureLoader();
 const colorTexture = textureLoader.load("/texture/color.jpg");
-// const colorTwo = textureLoader.load("/texture/normal.jpg");
+const matCapTexture = textureLoader.load("/texture/mat2.png");
+const bumpTexture = textureLoader.load("/texture/bump.jpg");
+const displacementTexture = textureLoader.load("/texture/displacementMap.jpg");
+
+// Cube TextureLoader
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+const envTexture = cubeTextureLoader.load([
+  "/texture/env/px.png",
+  "texture/env/nx.png",
+  "texture/env/py.png",
+  "texture/env/ny.png",
+  "texture/env/pz.png",
+  "texture/env/nz.png",
+]);
+scene.background = envTexture;
 
 //Resizing
 window.addEventListener("resize", () => {
@@ -45,8 +49,41 @@ window.addEventListener("resize", () => {
 });
 
 //Mesh
-const geometry = new THREE.PlaneGeometry(1, 1);
-const material = new THREE.MeshBasicMaterial({ map: colorTexture });
+const geometry = new THREE.SphereGeometry(0.5, 32, 32);
+// const geometry = new THREE.TorusGeometry(0.3, 0.2, 32, 32);
+const material = new THREE.MeshStandardMaterial({
+  metalness: 0.9,
+  roughness: 0.1,
+  envMap: envTexture,
+  //   shininess: 200,
+  //   specular: new THREE.Color("green"),
+  //   matcap: matCapTexture,
+  //   map: colorTexture,
+  //   bumpMap: bumpTexture,
+  //   displacementMap: displacementTexture,
+  //   wireframe: true,
+  //   color: new THREE.Color("skyblue"),
+  //   transparent: true,
+  //   opacity: 0.4,
+  //   side: THREE.DoubleSide,
+  //   visible: false,
+});
+// material.map = colorTexture;
+// material.wireframe = true;
+// material.color = new THREE.Color(0xff0000);
+// material.opacity = 0.4
+// material.transparent = true
+// material.side = THREE.FrontSide
+// material.visible = false
+// material.matcap = matcapTexture
+// material.shininess = 200
+// material.specular = 200
+
+// const geometry2 = new THREE.PlaneGeometry(1, 1, 64, 64);
+// const material2 = new THREE.MeshBasicMaterial();
+// const mesh2 = new THREE.Mesh(geometry, material);
+// mesh2.position.z = -1;
+
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
