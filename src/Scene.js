98e09469884acb443c3065f82e17a14bd4e78087
OGEmanuel <1blacksanta416@gmail.com>
2024-07-21 13:11:04 +0100

    Loading 3D complete

--- src/Scene.js
+++ src/Scene.js
@@ -1,16 +1,37 @@
 import { OrbitControls } from "@react-three/drei";
-import Particles from "./Particles";
+import Model from "./Model";
+import { Suspense } from "react";
+import Bike from "./Bike";
 
 const Scene = () => {
   return (
     <>
+      {/* <mesh>
+        <planeGeometry args={[4, 4]} />
+        <meshBasicMaterial />
+      </mesh> */}
+      <ambientLight intensity={2} />
+      <directionalLight />
       <OrbitControls />
 
       {/* <mesh>
-        <planeGeometry />
+        <boxGeometry />
         <meshBasicMaterial />
       </mesh> */}
-      <Particles />
+
+      <Suspense
+        fallback={
+          <mesh scale-y={2}>
+            <boxGeometry />
+            <meshBasicMaterial wireframe />
+          </mesh>
+        }
+      >
+        {/* {true ? <Model /> : ""} */}
+        <Model />
+      </Suspense>
+
+      {/* <Bike scale={0.85} position={[-0.5, 0.75, 0]} /> */}
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,18 +2,14 @@ import ReactDOM from "react-dom/client";
 import "./styles.css";
 import { Canvas } from "@react-three/fiber";
 import Scene from "./Scene";
+import { Perf } from "r3f-perf";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <Canvas
-      camera={{
-        fov: 45,
-        near: 0.01,
-        far: 100,
-      }}
-    >
+    <Canvas>
       <Scene />
+      <Perf position="top-left" />
     </Canvas>
   </>
 );
