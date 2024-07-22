e7ba8430033e59e4f88e1d48fddd7e9d503ffe08
OGEmanuel <1blacksanta416@gmail.com>
2024-07-22 03:31:20 +0100

    Complete

--- src/Scene.js
+++ src/Scene.js
@@ -1,62 +1,24 @@
-import {
-  Center,
-  Float,
-  Html,
-  OrbitControls,
-  Text,
-  Text3D,
-} from "@react-three/drei";
-import { useRef } from "react";
+import { OrbitControls, PositionalAudio } from "@react-three/drei";
+import { useState } from "react";
 
 const Scene = () => {
-  const cubeRef = useRef();
+  const [play, setPlay] = useState(false);
+
+  const clickHandler = () => {
+    setPlay(!play);
+  };
+
   return (
     <>
       <OrbitControls />
-      {/* <Text
-        fontSize={0.4}
-        color={"orange"}
-        font="./fonts/1.ttf"
-        position-y={Math.PI * 0.1}
-        rotation-y={Math.PI * 0.1}
-        maxWidth={2}
-        textAlign="center"
-      >
-        This is a Text
-      </Text>
 
-      <Center>
-        <Float speed={5} floatIntensity={4}>
-          <Text3D
-            font={"./fonts/2.json"}
-            height={1}
-            size={1.1}
-            letterSpacing={-0.1}
-            bevelEnabled
-            bevelSegments={20}
-          >
-            Hello
-            <meshNormalMaterial />
-          </Text3D>
-        </Float>
-      </Center> */}
+      {play && (
+        <PositionalAudio url="./sound/sound.mp3" autoplay loop distance={5} />
+      )}
 
-      <mesh position-x={1} ref={cubeRef}>
+      <mesh onClick={clickHandler}>
         <boxGeometry />
-        <meshBasicMaterial color={"orange"} />
-        <Html
-          position={[-0.7, 0.5, 0.5]}
-          wrapperClass="text"
-          distanceFactor={5}
-          occlude={[cubeRef]}
-        >
-          RTF
-        </Html>
-      </mesh>
-
-      <mesh position-x={-1}>
-        <boxGeometry />
-        <meshBasicMaterial color={"purple"} />
+        <meshBasicMaterial color="purple" />
       </mesh>
     </>
   );
