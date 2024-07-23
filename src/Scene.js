0f8dea8653f72d688b08ef09053442fc11254b99
OGEmanuel <1blacksanta416@gmail.com>
2024-07-23 03:36:45 +0100

    This is actually a springref branch

--- src/Scene.js
+++ src/Scene.js
@@ -1,46 +1,34 @@
-import { useState } from "react";
-import { a, useSpring } from "@react-spring/three";
-import { useFrame } from "@react-three/fiber";
+import { a, useSpring, useSpringRef } from "@react-spring/three";
 
 const Scene = () => {
-  // const [click, setClick] = useState(false);
-  useFrame(() => {
-    console.log(spring.x.get());
+  // const [spring, api] = useSpring(() => ({
+  //   from: { x: -2 },
+  // }));
+
+  const springRef = useSpringRef();
+
+  const spring = useSpring({
+    ref: springRef,
+    from: { x: -2 },
   });
 
-  // const { scale, color } = useSpring({
-  //   from: { scale: click ? 1 : 2, color: click ? "orange" : "hotpink" },
-  //   scale: click ? 2 : 1,
-  //   color: click ? "hotpink" : "orange",
-  // });
-
-  // console.log(scale);
-
-  // const clickHandler = () => {
-  //   setClick(!click);
-  // };
-
-  const [spring, api] = useSpring(() => ({
-    from: { x: 0 },
-  }));
-
-  const handleClick = () => {
-    api.start({
-      to: { x: spring.x.get() === 1 ? 0 : 1 },
+  const clickHandler = () => {
+    springRef.start({
+      to: { x: 2 },
+      config: { duration: 5000 },
     });
   };
 
-  console.log(spring);
-
   return (
     <>
-      {/* <a.mesh onClick={clickHandler} scale={scale}>
+      <a.mesh
+        position-x={spring.x}
+        onPointerOver={() => springRef.pause()}
+        onPointerOut={() => springRef.resume()}
+        onClick={clickHandler}
+      >
         <boxGeometry />
-        <a.meshBasicMaterial color={color} />
-      </a.mesh> */}
-      <a.mesh onClick={handleClick} position-x={spring.x}>
-        <boxGeometry />
-        <a.meshBasicMaterial color="orange" />
+        <meshBasicMaterial color="orange" />
       </a.mesh>
     </>
   );
