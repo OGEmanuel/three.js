915afb785eaec48140afd6ebcd039670168455e3
OGEmanuel <1blacksanta416@gmail.com>
2024-07-24 03:15:37 +0100

    Complete

--- src/PhysicsScene.js
+++ src/PhysicsScene.js
@@ -1,48 +1,45 @@
-import { Physics, RigidBody } from "@react-three/rapier";
+import { CuboidCollider, Debug, Physics, RigidBody } from "@react-three/rapier";
 import { useRef } from "react";
-
 const PhysicsScene = () => {
-  const cubeRef = useRef();
+  const firstCubeRef = useRef();
   const secondCubeRef = useRef();
+
+  const firstCubeClickHandler = () => {
+    firstCubeRef.current.applyImpulse({ x: -100, y: 0, z: 0 });
+  };
+
+  const secondCubeClickHandler = () => {
+    secondCubeRef.current.applyImpulse({ x: 100, y: 0, z: 0 });
+  };
+
   return (
     <>
       <Physics>
-        <RigidBody
-          ref={cubeRef}
-          onCollisionEnter={() => console.log("Collision enter")}
-          onCollisionExit={() => console.log("Collision exit")}
-          onSleep={() => console.log("Sleeping")}
-          onWake={() => console.log("Wake")}
-          gravityScale={1}
-          restitution={1}
-          friction={1}
-        >
+        <Debug />
+        <RigidBody ref={firstCubeRef} mass={20}>
           <mesh
             castShadow
             position={[1.5, 2.5, 0]}
-            onClick={() =>
-              cubeRef.current.applyTorqueImpulse({ x: 0, y: 5, z: 0 })
-            }
+            onClick={firstCubeClickHandler}
+            scale={3}
           >
             <boxGeometry />
             <meshStandardMaterial color="#CC3941" />
           </mesh>
         </RigidBody>
 
-        <RigidBody ref={secondCubeRef}>
+        <RigidBody ref={secondCubeRef} mass={200}>
           <mesh
             castShadow
             position={[-1.5, 2.5, 0]}
-            onClick={() =>
-              secondCubeRef.current.applyImpulse({ x: 8, y: 0, z: 0 })
-            }
+            onClick={secondCubeClickHandler}
           >
             <boxGeometry />
             <meshStandardMaterial color="#CC3941" />
           </mesh>
         </RigidBody>
 
-        <RigidBody type="fixed">
+        <RigidBody type="fixed" restitution={0.2}>
           <mesh position-y={-1} rotation-x={-Math.PI * 0.5} receiveShadow>
             <boxGeometry args={[15, 15, 0.35]} />
             <meshStandardMaterial color="#C7CAC7" />
